64b6d842781cf226489c8dcbf48c78fd3e966c35
Flikter <83476766+Flikter@users.noreply.github.com>
2025-09-22 15:44:21 +0200

    chore: cleanup

--- Program.cs
+++ Program.cs
@@ -1,81 +1,133 @@
-﻿using System;
-using System.IO;
-using System.IO.Compression;
+﻿using System.IO.Compression;
 using Microsoft.Win32;
-using System.Net.Http;
-using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 class Program
 {
     const string API_URL = "https://api.github.com/repos/luau-lang/luau/releases/latest";
     const string ZIP_FILE_NAME = "luau-windows.zip";
+    const string INSTALLATION_FOLDER_NAME = ".luau";
 
-    static async Task Main(string[] args)
+    static void Main(string[] args)
     {
-        string userFolder = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-        string targetFolder = Path.Combine(userFolder, ".luau");
-
-        if (args.Length > 0 && args[0] == "--uninstall")
+        // Stop if OS is not Windows
+        if (!OperatingSystem.IsWindows())
         {
-            Uninstall(targetFolder);
+            Console.WriteLine("This application is designed to run only on Windows.");
+            Console.ReadKey();
             return;
         }
 
-        string? downloadUrl = await GetLatestReleaseUrl();
-        if (downloadUrl == null)
+        string userFolder = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+        string installationFolder = Path.Combine(userFolder, INSTALLATION_FOLDER_NAME);
+
+        // Install if no arguments or install argument
+        if (args.Length == 0 || args[0] == "--install")
         {
-            Console.WriteLine("Error: Could not fetch the latest release download URL.");
+            Install(installationFolder);
+            Console.ReadKey();
+            return;
+        }
+
+        // Uninstall if uninstall argument
+        if (args[0] == "--uninstall")
+        {
+            Uninstall(installationFolder);
+            Console.ReadKey();
+            return;
+        }
+    }
+
+    static bool IsInstalled(string installationFolder)
+    {
+        return Directory.Exists(installationFolder);
+    }
+
+    static async void Install(string installationFolder)
+    {
+        // Prevent if already installed
+        if (IsInstalled(installationFolder))
+        {
+            Console.WriteLine("An existing installation was found. Aborting...");
+            return;
+        }
+
+        Console.WriteLine("Beginning installation...");
+
+        string? releaseURL = await GetReleaseURL();
+
+        if (releaseURL == null)
+        {
+            Console.WriteLine("Failed to get the release URL.");
             return;
         }
 
         string tempZipPath = Path.Combine(Path.GetTempPath(), ZIP_FILE_NAME);
 
-        if (!Directory.Exists(targetFolder))
-        {
-            Directory.CreateDirectory(targetFolder);
-        }
+        Console.WriteLine("Downloading release...");
+        await DownloadFile(releaseURL, tempZipPath);
 
-        DeleteAllFilesInDirectory(targetFolder);
-        await DownloadFile(downloadUrl, tempZipPath);
-        UnzipFile(tempZipPath, targetFolder);
+        Console.WriteLine("Extracting files...");
+        Directory.CreateDirectory(installationFolder);
+        ZipFile.ExtractToDirectory(tempZipPath, installationFolder);
         File.Delete(tempZipPath);
-        AddDirectoryToPath(targetFolder);
 
-        Console.WriteLine("Luau latest release installed successfully.");
+        Console.WriteLine($"Adding {INSTALLATION_FOLDER_NAME} folder to PATH...");
+        AddFolderToPath(installationFolder);
+
+        Console.WriteLine("Installation complete.");
     }
 
-    static void Uninstall(string targetFolder)
+    static async void Uninstall(string installationFolder)
     {
-        if (Directory.Exists(targetFolder))
+        // Prevent if already uninstalled
+        if (!IsInstalled(installationFolder))
         {
-            Directory.Delete(targetFolder, true);
-            Console.WriteLine("Deleted .luau directory.");
+            Console.WriteLine("An installation was not found. Aborting...");
+            return;
         }
 
-        string? currentPath = Environment.GetEnvironmentVariable("PATH", EnvironmentVariableTarget.User);
-        if (currentPath != null && currentPath.Contains(targetFolder))
+        Console.WriteLine("Uninstalling...");
+
+        if (Directory.Exists(installationFolder))
         {
-            string newPath = currentPath.Replace(targetFolder + ";", "");
+            Console.WriteLine($"Deleting {INSTALLATION_FOLDER_NAME} folder...");
+            Directory.Delete(installationFolder, true);
+        }
+
+        Console.WriteLine($"Deleting {INSTALLATION_FOLDER_NAME} folder from PATH...");
+
+        string? currentPath = Environment.GetEnvironmentVariable("PATH", EnvironmentVariableTarget.User);
+
+        if (currentPath != null && currentPath.Contains(installationFolder))
+        {
+            string newPath = currentPath.Replace(installationFolder + ";", "");
             Environment.SetEnvironmentVariable("PATH", newPath, EnvironmentVariableTarget.User);
-            Console.WriteLine("Removed .luau from PATH.");
         }
 
         RegistryKey? key = Registry.CurrentUser.OpenSubKey("Environment", true);
-        key?.DeleteValue("PATH", false);
-        key?.Close();
+
+        if (key != null)
+        {
+            key.DeleteValue("PATH", true);
+            key.Close();
+        }
 
         Console.WriteLine("Uninstallation complete.");
     }
 
-    static async Task<string?> GetLatestReleaseUrl()
+    static async Task<string?> GetReleaseURL()
     {
         var client = new HttpClient();
+
+        // Needed, otherwise 403
         client.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0");
 
         try
         {
             var response = await client.GetStringAsync(API_URL);
-            var releaseData = Newtonsoft.Json.Linq.JObject.Parse(response);
+            var releaseData = JObject.Parse(response);
+
             foreach (var asset in releaseData["assets"]!)
             {
                 if (asset["name"]?.ToString() == ZIP_FILE_NAME)
@@ -86,8 +138,9 @@ class Program
         }
         catch (Exception ex)
         {
-            Console.WriteLine($"Error fetching release data: {ex.Message}");
+            Console.WriteLine(ex.Message);
         }
+
         return null;
     }
 
@@ -100,31 +153,14 @@ class Program
         stream.CopyTo(file);
     }
 
-    static void UnzipFile(string zipFilePath, string targetDirectory)
-    {
-        ZipFile.ExtractToDirectory(zipFilePath, targetDirectory);
-    }
-
-    static void DeleteAllFilesInDirectory(string directory)
-    {
-        foreach (var file in Directory.GetFiles(directory))
-        {
-            File.Delete(file);
-        }
-        foreach (var subdir in Directory.GetDirectories(directory))
-        {
-            Directory.Delete(subdir, true);
-        }
-    }
-
-    static void AddDirectoryToPath(string directory)
+    static void AddFolderToPath(string folder)
     {
         string? currentPath = Environment.GetEnvironmentVariable("PATH", EnvironmentVariableTarget.User);
-        if (currentPath == null || !currentPath.Contains(directory))
+
+        if (currentPath == null || !currentPath.Contains(folder))
         {
-            string newPath = (currentPath == null) ? directory : currentPath + ";" + directory;
+            string newPath = (currentPath == null) ? folder : currentPath + ";" + folder;
             Environment.SetEnvironmentVariable("PATH", newPath, EnvironmentVariableTarget.User);
-            Console.WriteLine("Added .luau to PATH.");
         }
     }
 }
